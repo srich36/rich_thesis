f5a77b28e5df21281fffaad5a86272ea3bc9177a
Sean Rich <srich366@gmail.com>
2020-01-24 11:08:01 -0500

    Correctly implement intial integration step

--- c++/src/singleThreaded/main.cpp
+++ c++/src/singleThreaded/main.cpp
@@ -16,8 +16,8 @@ typedef std::vector< double > state_type;
 /*
 ************Configuration params************
 */
-const int numExecutions=1, numPsoIterations=1, numParticles = 100;
-const int numUnknowns = 11, numIterations = 500;
+const int numExecutions=1, numPsoIterations=1, numParticles = 1;
+const int numUnknowns = 11, numIterations = 1;
 /*
 ************Configuration params************
 */
@@ -80,25 +80,77 @@ struct thrustArcIc{
     }
 };
 
-void thrustArc1EOM( const state_type &x , state_type &dxdt , const double t, double ub, fuelParams fuel,
-                    double xi0, double xi1, double xi2, double xi3  )
-{
-    double delta = xi0+xi1*t+xi2*pow(t,2)+xi3*pow(t,3);
-    double ratio = (fuel.c*fuel.n0)/(fuel.c-fuel.n0*t);
-    dxdt[0] = -1*(ub-x[3]*pow(x[2],2))/(pow(x[3],2))+ratio*sin(delta);
-    dxdt[1] = -1*x[1]*x[2]/x[3]+ratio*cos(delta);
-    dxdt[2] = x[1];
-    dxdt[3] = x[2]/x[3];
-}
+class thrustArc1EOM {
+
+public:
+    double ub, xi0, xi1, xi2, xi3;
+    fuelParams fuel;
+
+    thrustArc1EOM(double ub, double xi0, double xi1, double xi2, double xi3, fuelParams fuel){
+        this->ub=ub;
+        this->xi0 = xi0;
+        this->xi1 = xi1;
+        this->xi2 = xi2;
+        this->xi3 = xi3;
+        this->fuel = fuel;
+    }
+
+    void operator()( const state_type &x , state_type &dxdt , const double t)
+    {
+        //Delta is working correctly
+        double delta = xi0+xi1*t+xi2*pow(t,2)+xi3*pow(t,3);
+
+        double ratio = (fuel.c*fuel.n0)/(fuel.c-fuel.n0*t);
+
+        //This integrates correctly for a good particle
+
+        dxdt[0] = -1*(ub-x[2]*pow(x[1],2))/(pow(x[2],2))+ratio*sin(delta);
+        dxdt[1] = -1*x[0]*x[1]/x[2]+ratio*cos(delta);
+        dxdt[2] = x[0];
+        dxdt[3] = x[1]/x[2];
+    }
+};
+
+class thrustArc2EOM {
+
+public:
+    double ub, v0, v1, v2, v3, deltaT1;
+    fuelParams fuel;
+
+    thrustArc2EOM(double ub, double xi0, double xi1, double xi2, double xi3, fuelParams fuel, double deltaT1){
+        this->ub=ub;
+        this->v0 = xi0;
+        this->v1 = xi1;
+        this->v2 = xi2;
+        this->v3 = xi3;
+        this->fuel = fuel;
+        this->deltaT1 = deltaT1;
+    }
+
+    void operator()( const state_type &x , state_type &dxdt , const double t)
+    {
+        //Delta is working correctly
+        double delta = v0+v1*t+v2*pow(t,2)+v3*pow(t,3);
+
+        double ratio = (fuel.c*fuel.n0)/(fuel.c-fuel.n0*(t+deltaT1));
+
+        //This integrates correctly for a good particle
+
+        dxdt[0] = -1*(ub-x[2]*pow(x[1],2))/(pow(x[2],2))+ratio*sin(delta);
+        dxdt[1] = -1*x[0]*x[1]/x[2]+ratio*cos(delta);
+        dxdt[2] = x[0];
+        dxdt[3] = x[1]/x[2];
+    }
+};
 
 void thrustArc2EOM( const state_type &x , state_type &dxdt , const double t, double ub, fuelParams fuel,
                     double v0, double v1, double v2, double v3, double t1  ) {
     double delta = v0+v1*t+v2*pow(t,2)+v3*pow(t,3);
     double ratio = fuel.c*fuel.n0/(fuel.c-fuel.n0*(t1+t));
-    dxdt[0] = -1*(ub-x[3]*pow(x[2],2))/(pow(x[3],2))+ratio*sin(delta);
-    dxdt[1] = -1*x[1]*x[2]/x[3]+ratio*cos(delta);
-    dxdt[2] = x[1];
-    dxdt[3] = x[2]/x[3];
+    dxdt[0] = -1*(ub-x[2]*pow(x[1],2))/(pow(x[2],2))+ratio*sin(delta);
+    dxdt[1] = -1*x[0]*x[1]/x[2]+ratio*cos(delta);
+    dxdt[2] = x[0];
+    dxdt[3] = x[1]/x[2];
 
 }
 
@@ -114,17 +166,8 @@ double testParticle[] = {-0.856580908970521,-0.928947964866100,-0.53219453719283
 
 
 bool evalTestParticle = false;
-bool debug = false;
+bool debug = true;
 bool debugPSO = false;
-
-/*
-************Rehydration params************
-*/
-bool rehydrate = false;
-double averageJThresholdPercent = .1;
-int numIterBeforeHydration = 100;
-int numPrevIterationsAvgJ = 50;
-double rehydratePercentage = .33;
 
 /*
 ************Display params************
@@ -161,7 +204,6 @@ int main(){
     srand(time(0));
     int Beta = 2;
 
-auto rkd = runge_kutta_dopri5<state_type>{};
 
     /* Initial condition set */
     double vrInitial = 0; double vrTerminal = 0; double xiInital = 0;
@@ -223,22 +265,129 @@ auto rkd = runge_kutta_dopri5<state_type>{};
 
             //Evaluate each particle here
             double deltaT1Particle = swarm[indexConversion(particleNum, 9, numUnknowns)];
+            double xi0 = swarm[indexConversion(particleNum, 0, numUnknowns)];
+            double xi1 = swarm[indexConversion(particleNum, 1, numUnknowns)];
+            double xi2 = swarm[indexConversion(particleNum, 2, numUnknowns)];
+            double xi3 = swarm[indexConversion(particleNum, 3, numUnknowns)];
+
+            //Test Particle;
+            //-0.094683	0.088429	-0.015286	0.56713	-0.12919	0.13894	0.22764	0.67635	0.66978	2.7559	0.41389;
+
+            /*Remove this when doing the whole swarm*/
+            xi0 = -.094683;
+            xi1 = .088429;
+            xi2 = -.015286;
+            xi3 = .56713;
+            deltaT1Particle = .66978;
+            /*Remove this when doing the whole swarm*/
+
 
             state_type inoutTarc1 = { tarc1.vrInitial, tarc1.vThetaInital, tarc1.rInitial, tarc1.xiInital};
-            double t_start = 0.0 , t_end = 1.0, dt=.001;
+            double t_start = 0.0 , t_end = deltaT1Particle, dt=.0001;
             //[ dense_output_detail_generation1
             typedef boost::numeric::odeint::result_of::make_dense_output<
                 runge_kutta_dopri5< state_type > >::type dense_stepper_type;
-            dense_stepper_type dense2 = make_dense_output( 1.0e-6 , 1.0e-6 , runge_kutta_dopri5< state_type >() );
-            //]
 
-            //[ dense_output_detail_generation2
+            dense_stepper_type dense2 = make_dense_output( 1.0e-6 , 1.0e-6 , runge_kutta_dopri5< state_type >() );
 
             /*
                 @parameter absolute error tolerance
                 @parameter relative error tolerance
             */
-            integrate_const( make_dense_output( 1.0e-6 , 1.0e-6 , runge_kutta_dopri5< state_type >() ) , thrustArc1EOM , inout , t_start , t_end , dt );
+            thrustArc1EOM sys1 = thrustArc1EOM(ub, xi0, xi1, xi2, xi3, fuel );
+            integrate_const( dense2 , sys1 , inoutTarc1 , t_start , t_end , dt );
+            cout << inoutTarc1[0] << " " << inoutTarc1[1] << " " << inoutTarc1[2] << " " << inoutTarc1[3] << endl;
+            double vr1 = inoutTarc1[0];
+            double vTheta1 = inoutTarc1[1];
+            double r1 = inoutTarc1[2];
+            double xi1PostTarc = inoutTarc1[3];
+            double aCoast = ub*r1/(2*ub-r1*(pow(vr1,2)+pow(vTheta1,2)));
+            double eCoast = sqrt(1-pow(r1,2)*pow(vTheta1,2)/(ub*aCoast));
+
+            if(debug){
+                cout << "vr1 is " << vr1 << endl;
+                cout << "vTheta1 is " << vTheta1 << endl;
+                cout << "r1 is " << r1 << endl;
+                cout << "xi1 is " << xi1 << endl;
+                cout << "aCoast is " << aCoast << endl;
+                cout << "e Coast is " << eCoast << endl << endl;
+            }
+
+            if(aCoast > 0){
+                double sinTrueAnamoly1 = vr1/eCoast*sqrt(aCoast*(1-pow(eCoast,2))/ub);
+                double cosTrueAnamoly1 = vTheta1/eCoast*sqrt(aCoast*(1-pow(eCoast,2))/ub)-1/eCoast;
+                double trueAnamoly1 = atan2(sinTrueAnamoly1, cosTrueAnamoly1);
+                if (trueAnamoly1 < 0){
+                    trueAnamoly1=trueAnamoly1+2*M_PI;
+                }
+
+                double sinEccAnomaly1 = sin(trueAnamoly1)*sqrt(1-pow(eCoast,2))/(1+eCoast*cosTrueAnamoly1);
+                double cosEccAnomaly1 = (cos(trueAnamoly1)+eCoast)/(1+eCoast*cosTrueAnamoly1);
+                double eccAnamoly1 = atan2(sinEccAnomaly1, cosEccAnomaly1);
+                if (eccAnamoly1 < 0) {
+                    eccAnamoly1=eccAnamoly1+2*M_PI;
+                }
+
+                double deltaE = swarm[indexConversion(particleNum, 9, numUnknowns )];
+
+                /* Remove this when doing the whole swarm*/
+                deltaE = 2.7559;
+                /* Remove this when doing the whole swarm*/
+
+
+                double eccAnomaly2 = eccAnamoly1+deltaE;
+
+                double sinTrueAnamoly2 = sin(eccAnomaly2)*sqrt(1-pow(eCoast,2))/(1-eCoast*cos(eccAnomaly2));
+                double cosTrueAnamoly2 = (cos(eccAnomaly2)-eCoast)/(1-eCoast*cos(eccAnomaly2));
+
+                double trueAnamoly2 = atan2(sinTrueAnamoly2, cosTrueAnamoly2);
+
+                if ( trueAnamoly2 < 0){
+                    trueAnamoly2=trueAnamoly2+2*M_PI;
+                }
+                double coastingTimeInterval = sqrt(pow(aCoast,3)/ub)*(eccAnomaly2-eccAnamoly1-eCoast*(sin(eccAnomaly2)-sin(eccAnamoly1)));
+
+                //The first section is checked and working for good particles
+                if(debug){
+                    cout << "True anamoly 1 is " << trueAnamoly1 << endl;
+                    cout << "Eccentric anamoly 1 is " << eccAnamoly1 << endl;
+                    cout << "Eccentric anamoly 2 is " << eccAnomaly2 << endl;
+                    cout << "True anamoly 2 is " << trueAnamoly2 << endl;
+                    cout << "Coasting time interval 2 is " << coastingTimeInterval << endl;
+                }
+
+                double vr2 = sqrt(ub/(aCoast*(1-pow(eCoast,2))))*eCoast*sin(trueAnamoly2);
+                double vtheta2 = sqrt(ub/(aCoast*(1-pow(eCoast,2))))*(1+eCoast*cos(trueAnamoly2));
+                double r2 = aCoast*(1-pow(eCoast,2))/(1+eCoast*cos(trueAnamoly2));
+                double xi2PreTarc2 = xi1PostTarc + (trueAnamoly2-trueAnamoly1);
+
+                //This is checked and working
+                if(debug){
+                    cout << "vr2 is " << vr2 << endl;
+                    cout << "vTheta2 is " << vtheta2 << endl;
+                    cout << "r2 is " << r2 << endl;
+                    cout << "xi2 is " << xi2PreTarc2 << endl;
+                }
+
+                double deltaT2Particle = swarm[indexConversion(particleNum, 10, numUnknowns )];
+                double v0 = swarm[indexConversion(particleNum, 4, numUnknowns)];
+                double v1 = swarm[indexConversion(particleNum, 5, numUnknowns)];
+                double v2 = swarm[indexConversion(particleNum, 6, numUnknowns)];
+                double v3 = swarm[indexConversion(particleNum, 7, numUnknowns)];
+
+                //Test Particle;
+                //-0.094683	0.088429	-0.015286	0.56713	-0.12919	0.13894	0.22764	0.67635	0.66978	2.7559	0.41389;
+
+                /* Remove this when doing the whole swarm */
+                v0 = -.12919;
+                v1 = .13894;
+                v2 = .22765;
+                v3 = .67635;
+                deltaT2Particle = .41389;
+                /* Remove this when doing the whole swarm */
+
+
+            }
 
 
            }
